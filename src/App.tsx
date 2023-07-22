567c52f7a2e0d9a1247600a2f130445c89a08d3b
yymin1022 <yymin1022@gmail.com>
2023-07-22 15:48:53 +0900

    Update App.tsx : Added Recaptcha Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
+
 import './App.css';
 
 const App = () => {
     return (
         <div>
-            Hello
+            <ReCAPTCHA
+                sitekey={process.env.REACT_APP_GRECAPTCHA_KEY!}
+                onChange={onChange} />
         </div>
     );
+}
+
+const onChange = (e: any) => {
+    console.log(e.toString());
 }
 
 export default App;
