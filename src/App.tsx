b9fb13d0e8be3bebffb5a31631f5ab5fc11cc578
yymin1022 <yymin1022@gmail.com>
2023-07-22 18:33:23 +0900

    Update App.tsx : Added Stopwatch Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReCAPTCHA from "react-google-recaptcha";
 
 import './App.css';
@@ -6,6 +6,7 @@ import './App.css';
 const App = () => {
     return (
         <div>
+            <StopWatch />
             <ReCAPTCHA
                 sitekey={process.env.REACT_APP_GRECAPTCHA_KEY!}
                 onChange={onChange} />
@@ -17,10 +18,43 @@ const onChange = (e: any) => {
     console.log(e.toString());
 }
 
+let isStart = false;
+let timer: NodeJS.Timer;
+
 const StopWatch = () => {
+    const [curTimeStr, setCurTimeStr] = useState("00:00.000");
+
+    let curTime = 0;
+
+    const startStopwatch = () => {
+        if(!isStart) {
+            isStart = true;
+            timer = setInterval(() => {
+                if(isStart) {
+                    curTime++;
+                    setCurTimeStr(new Date(curTime * 10).toISOString().slice(14, 22));
+                }
+            }, 10);
+        }
+    }
+
+    const stopStopwatch = () => {
+        if(isStart) {
+            console.log(curTimeStr);
+
+            curTime = 0;
+            isStart = false;
+
+            clearInterval(timer);
+            setCurTimeStr("00:00.000");
+        }
+    }
+
     return (
         <>
-            Stopwatch
+            <p>{curTimeStr}</p>
+            <button onClick={startStopwatch}>Start!</button>
+            <button onClick={stopStopwatch}>Stop!</button>
         </>
     )
 }
