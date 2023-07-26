6653b9c6b7b359bf9594afe0f2424336b8ee04ca
yymin1022 <yymin1022@gmail.com>
2023-07-27 02:46:32 +0900

    Added KoreanTranslate Component

---
+++ src/KoreanTranslate.tsx
@@ -0,0 +1,11 @@
+import {ComponentProps} from "react";
+
+const KoreanTranslate = (props: ComponentProps<any>) => {
+    return (
+        <>
+            <p>Korean Translate !!</p>
+        </>
+    )
+}
+
+export default KoreanTranslate;
