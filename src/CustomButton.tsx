887bbdadbb1fe5f0461d09de351ed6d864b9331a
yymin1022 <yymin1022@gmail.com>
2023-07-27 22:41:57 +0900

    Update CustomButton : Adjust Styling

--- src/CustomButton.tsx
+++ src/CustomButton.tsx
@@ -2,6 +2,30 @@ import styled from "styled-components";
 
 const CustomButton = styled.button`
     appearance: none;
+
+    margin: 10px;
+    padding: 0.5rem 1rem;
+    
+    font-size: 1rem;
+    font-weight: 400;
+    text-align: center;
+    text-decoration: none;
+    
+    display: inline-block;
+    width: auto;
+    
+    border: none;
+    border-radius: 4px;
+
+    background: #DDDDDD;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    cursor: pointer;
+    transition: 0.5s;
+
+    &:hover {
+      background: #BBBBBB;
+      outline: 0;
+    }
 `;
 
 export default CustomButton;
